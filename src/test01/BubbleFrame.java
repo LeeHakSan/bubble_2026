27c535a1f4d8b33ee74b7a5483df547663758583
이학산 <san1425865@gmail.com>
2026-03-12 12:06:10 +0900

    feat : bubble movement

---
+++ src/test01/BubbleFrame.java
@@ -0,0 +1,75 @@
+package test01;
+
+import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class BubbleFrame extends JFrame {
+
+    private JLabel backgroundMap;
+    private Player player;
+
+    public BubbleFrame() {
+        initData();
+        setInitLayout();
+        addEventListener();
+    }
+
+    private void initData() {
+        setTitle("버블버블 게임");
+        setSize(1000, 640);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        backgroundMap = new JLabel(new ImageIcon("img/backgroundMap.png"));
+        setContentPane(backgroundMap);
+
+        player = new Player();
+    }
+
+    private void setInitLayout() {
+        setLayout(null);
+        setResizable(false); // 창 크기 고정
+        setLocationRelativeTo(null); // 화면 정중앙 배치 (프레임)
+
+        backgroundMap.add(player);
+
+        setVisible(true);
+
+
+    }
+
+    private void addEventListener() {
+        this.addKeyListener(new KeyAdapter() {
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                switch (e.getKeyCode()) {
+                    // 방향키 코드를 player의 이동 메서드로 연결
+                    case KeyEvent.VK_LEFT:
+                        player.left();
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        player.right();
+                        break;
+                    case KeyEvent.VK_UP:
+                        player.up();
+                        break;
+
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                super.keyReleased(e);
+            }
+
+        });
+    }
+
+    // 테스트 코드
+    public static void main(String[] args) {
+        new BubbleFrame();
+    }
+}
+
+
