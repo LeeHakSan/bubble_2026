e11e5b4f6976c68430936ed3827f9b63d5536d31
이학산 <san1425865@gmail.com>
2026-03-13 10:06:27 +0900

    feat: 업데이트

--- src/test04/Bubble.java
+++ src/test04/Bubble.java
@@ -43,7 +43,7 @@ public class Bubble extends JLabel {
     private void setInitLayout() {
         x = player.getX();
         y = player.getY();
-
+        setIcon(bubbleIcon);
         setSize(50, 50);
         setLocation(x, y);
         setVisible(true);
@@ -51,16 +51,3 @@ public class Bubble extends JLabel {
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/test04/BubbleFrame.java
+++ src/test04/BubbleFrame.java
@@ -70,9 +70,25 @@ public class BubbleFrame extends JFrame {
                     case KeyEvent.VK_UP:
                         player.up();
                         break;
+                    case KeyEvent.VK_SPACE:
+                        // 버블 생성
+                        fireBubble();
+                        break;
+
                 }
             }
         });
+    }
+
+    //todo 임시 버블 클래스 생성
+    private void fireBubble () {
+        Bubble bubble = new Bubble(player);
+        backgroundMap.add(bubble);
+        // 동적으로 컴포넌트가 그려지기에 버그 발생 가능
+        backgroundMap.revalidate(); // 레이아웃 재계산
+        backgroundMap.repaint(); // 다시 그림
+
+
     }
 
     public static void main(String[] args) {
